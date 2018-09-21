469a4ff3572514c661f382514b2703092eb4654c
Evelyn Gálvez <evelyn.galvez.p@gmail.com>
2018-09-21 19:37:34 -0300

    agregado toolbar en componente principal con logo

--- social-network/src/app/app.module.ts
+++ social-network/src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//Bootstrap
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+//Material
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+//Toolbar
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
